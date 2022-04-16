cb76bb1e0c14a72ab0cde8dc6293b1f698fc0e55
Doyeop Kim <kdy980824@naver.com>
2022-04-16 22:03:43 +0900

    update CalcSim

--- src/scripts/searcher.java
+++ src/scripts/searcher.java
@@ -21,7 +21,7 @@ public class searcher {
 		this.input_file = file;
 	}
 
-	public void tmp(String query) throws Exception {
+	public void CalcSim(String query) throws Exception {
 
 		// query 분해
 		KeywordExtractor ke = new KeywordExtractor();
@@ -57,21 +57,26 @@ public class searcher {
 
 		double queryVectorSize = 0;
 		for (int i = 0; i < keywords.size(); i++) {
-			queryVectorSize += keyWeights.get(i);
+			queryVectorSize += keyWeights.get(i) * keyWeights.get(i);
 		}
 		queryVectorSize = Math.sqrt(queryVectorSize); // query vector 크기
 
-		for (int i = 0; i < docN; i++) { // 내적 계산
-			double innerProduct = 0;
+		for (int i = 0; i < docN; i++) {
 			double docVectorSize = 0;
 			for (int j = 0; j < keywords.size(); j++) {
 				String key = keywords.get(j);
-				double queryKeyWeight = (double) keyWeights.get(j);
-				double docKeyWeight = (double) ((ArrayList<Object>) hash.get(key)).get(i * 2 + 1);
-				innerProduct += queryKeyWeight * docKeyWeight; // 내적 결과
+				double docKeyWeight;
+
+				if (hash.get(key) == null) {
+					docKeyWeight = 0;
+				} else {
+					docKeyWeight = (double) ((ArrayList<Object>) hash.get(key)).get(i * 2 + 1);
+				}
 				docVectorSize += docKeyWeight * docKeyWeight;
 			}
 			docVectorSize = Math.sqrt(docVectorSize); // index.post의 keyweight vector 크기
+
+			double innerProduct = InnerProduct(keywords, keyWeights, hash, i);
 			double sim = innerProduct / (queryVectorSize * docVectorSize);
 			sims.add(sim);
 		}
@@ -102,78 +107,21 @@ public class searcher {
 		}
 	}
 
-	void CalcSim(String query) throws Exception {
-
-		// index.post 불러오기
-		FileInputStream InputfileStream = new FileInputStream(input_file);
-		ObjectInputStream objectInputStream = new ObjectInputStream(InputfileStream);
-		Object object = objectInputStream.readObject();
-		objectInputStream.close();
-
-		HashMap hashMap = (HashMap) object;
-
-		// index.xml 불러오기
-		DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
-		Document index = docBuilder.parse("index.xml");
-
-		int docN = index.getElementsByTagName("doc").getLength(); // 전체 문서 개수
-		NodeList titleList = index.getElementsByTagName("title"); // title 목록
-
-		// 쿼리 분해
-		KeywordExtractor ke = new KeywordExtractor();
-		KeywordList kl = ke.extractKeyword(query, true);
-
-		ArrayList<String> keys = new ArrayList<String>();
-		ArrayList<Integer> keyWeights = new ArrayList<Integer>();
-		ArrayList<Double> sims = new ArrayList<Double>();
-
-		for (int i = 0; i < kl.size(); i++) {
-			Keyword kwrd = kl.get(i);
-			keys.add(kwrd.getString());
-			keyWeights.add(kwrd.getCnt());
-		}
-
+	double InnerProduct(ArrayList<String> keys, ArrayList<Integer> keyWeights, HashMap hashMap, int docNumber)
+			throws Exception {
 		// 내적 계산
-		for (int i = 0; i < docN; i++) {
-			double sim = 0;
-			for (int j = 0; j < keys.size(); j++) {
-				String key = keys.get(j);
-				double keyWeight = keyWeights.get(j);
-				double hashWeight;
-				if (hashMap.get(key) == null) {
-					hashWeight = 0;
-				} else {
-					hashWeight = (double) ((ArrayList<Object>) hashMap.get(key)).get(i * 2 + 1);
-				}
-				sim += keyWeight * hashWeight;
-			}
-			sims.add(sim);
-		}
-
-		// 가장 높은 3개 출력
-		int maxIndex;
-		double maxValue;
-
-		for (int i = 0; i < 3; i++) {
-			maxIndex = -1;
-			maxValue = 0;
-			for (int j = 0; j < docN; j++) {
-				if (sims.get(j) > maxValue) {
-					maxIndex = j;
-					maxValue = sims.get(j);
-				}
-			}
-			if (maxValue != 0) {
-				System.out.print(titleList.item(maxIndex).getTextContent());
-				System.out.printf("(유사도:%.2f)\n", maxValue);
-				sims.set(maxIndex, 0.0);
+		double sim = 0;
+		for (int j = 0; j < keys.size(); j++) {
+			String key = keys.get(j);
+			double keyWeight = keyWeights.get(j);
+			double hashWeight;
+			if (hashMap.get(key) == null) {
+				hashWeight = 0;
 			} else {
-				if (i == 0) {
-					System.out.println("검색된 문서가 없습니다.");
-				}
-				break;
+				hashWeight = (double) ((ArrayList<Object>) hashMap.get(key)).get(docNumber * 2 + 1);
 			}
+			sim += keyWeight * hashWeight;
 		}
+		return sim;
 	}
 }
